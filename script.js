7acf2a6fd3caea907e0f720562b57e40adb0fb3f
Venilson1 <venilson2009@hotmail.com>
2020-07-01 08:50:34 -0300

    criar comida

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ snake[0] = {
 }
 
 let direction = "right"; //movimentação
+let food = {
+  x:Math.floor(Math.random() * 15 + 1) * box,
+  y:Math.floor(Math.random() * 15 + 1) * box
+}
+
 
 function criarBG() {
   context.fillStyle = "lightgreen"
@@ -20,6 +25,11 @@ function criarCobrinha() {
     context.fillStyle = "black"
     context.fillRect(snake[i].x, snake[i].y, box, box)
   }
+}
+
+function drawFood() {
+  context.fillStyle = 'red'
+  context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener('keydown', update);
@@ -36,15 +46,16 @@ function update(event) {
 function iniciarJogo(){
 
   if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0 
-  if (snake[0].x < 0 && direction == 'left') snake[0].x = 14 * box
+  if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box
   if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0 
-  if (snake[0].y < 0 && direction == 'up') snake[0].y = 14 * box
+  if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
   
   
 
 
   criarBG();
   criarCobrinha();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
